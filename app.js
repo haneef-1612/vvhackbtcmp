8d4139d7d948c61de97dbc0d2fc2df1fa6de7cbd
haneefshaik <haneefshaik2020@gmail.com>
2024-08-03 16:43:33 +0530

    hi

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById("sha").textContent="Shaik"
+document.getElementsByClassName("han")[0].textContent="hellooooo"
+document.getElementById("sha").innerHTML="<h1>HANEEF</h1>"
+document.querySelector(".han").textContent="Welcome"
+document.querySelectorAll(".han")[0].textContent="Home"
+
+const con = document.createElement("div");
+con.innerHTML="<h1>AMALAPURAM</h1>"
+document.body.appendChild(con);
---
+++ variables.js
@@ -0,0 +1,21 @@
+// let a=3;
+// console.log(typeof(a));
+// var b='han'
+// const c=222;
+// let obj={
+//     name:haneef
+// // }
+// let arr=[20, "shaik"]
+
+// console.log(typeof(arr));
+
+
+
+let data={
+    // name: "haneef",
+    age: 99,
+    specs: "yes"
+}
+
+console.log(data.name&&"no data");
+console.log(data.name||"no data");
